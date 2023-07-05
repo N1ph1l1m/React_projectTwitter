e33b8983b45e411951c47247f2ca1b39807b1098
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-05 22:08:30 +0300

    Add style with reactstrap for post-list and post-status-filter

--- app1/src/components/post-status-filter/post-status-filter.js
+++ app1/src/components/post-status-filter/post-status-filter.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from 'reactstrap';
 import "./post-status-filter.css";
 
 const PostStatusFilter = () =>{
     return (
         <div className="btn-group">
+            <Button outline color='info'>All2</Button>
             <button  type="button" className="btn btn-info">All</button>
             <button type="button" className="btn btn-secondary">Like it</button>
         </div>
