2cd0823d028f9f9594d762329cb2654b88004d11
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-05 20:00:04 +0300

    Create state for like

--- app1/src/components/post-list-item/post-list-item.js
+++ app1/src/components/post-list-item/post-list-item.js
@@ -4,27 +4,44 @@ export default class PostListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      important: false
+      important: false,
+      like:false
     };
     this.onImportant = this.onImportant.bind(this);
+    this.onLike = this.onLike.bind(this);
   }
 
-  onImportant() {
-    this.setState(({ important }) => ({
-      important: !important
-    }));
-  }
+ 
+    onImportant() {
+        this.setState(({ important }) => ({
+        important: !important
+        }));
+    }
 
+    onLike() {
+        this.setState(({ like }) => ({
+        like: !like
+        }));
+    }
   render() {
     const { label } = this.props;
-    const { important } = this.state;
+    const { important,like } = this.state;
     let classNames = "app-list-item d-flex justify-content-between";
     if (important) {
       classNames += " important";
+    } 
+    
+    if (like) {
+        classNames += " like";
     }
+
+
     return (
       <div className={classNames}>
-        <span className="app-list-item-label">{label}</span>
+        <span className="app-list-item-label"
+              onClick={this.onLike}>
+              {label}
+        </span>
         <div className="d-flex justify-content-center align-items-center">
           <button
             type="button"
