8fd174ceceaa932943c74cc88a0507951dccf88f
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-05 19:51:17 +0300

    Create state in post-list-item for stars

--- app1/src/components/post-list-item/post-list-item.js
+++ app1/src/components/post-list-item/post-list-item.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
-const PostListItem = ({label, important = false }) => {
+export default class PostListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      important: false
+    };
+    this.onImportant = this.onImportant.bind(this);
+  }
 
-    let classNames = 'app-list-item d-flex justify-content-between';
-    if(important){
-        classNames += ' important';
-    } 
-    return(
-        <div className={classNames}>
-            <span className="app-list-item-label">
-                {label}
-            </span>
-            <div className="d-flex justify-content-center align-items-center" >
-                <button 
-                    type="button" 
-                    className="btn-star btn-sm">
-                    <i className="bi bi-star"></i>
-                </button>
-                <button 
-                    type="button" 
-                    className="btn-trash btn-sm">
-                    <i className="bi bi-trash"></i>
-                </button>
-                <i className="bi bi-heart"></i>
-            </div>
+  onImportant() {
+    this.setState(({ important }) => ({
+      important: !important
+    }));
+  }
+
+  render() {
+    const { label } = this.props;
+    const { important } = this.state;
+    let classNames = "app-list-item d-flex justify-content-between";
+    if (important) {
+      classNames += " important";
+    }
+    return (
+      <div className={classNames}>
+        <span className="app-list-item-label">{label}</span>
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-star btn-sm"
+            onClick={this.onImportant}>
+            <i className="bi bi-star"></i>
+          </button>
+          <button type="button" className="btn-trash btn-sm">
+            <i className="bi bi-trash"></i>
+          </button>
+          <i className="bi bi-heart"></i>
         </div>
-    )
+      </div>
+    );
+  }
 }
-export default PostListItem;
