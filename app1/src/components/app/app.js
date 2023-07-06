7dc1765d2e6e01690a71749feff065e0a0cdfe51
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-06 20:40:22 +0300

    Delete post.Code status - work

--- app1/src/components/app/app.js
+++ app1/src/components/app/app.js
@@ -1,51 +1,53 @@
-import React , {Component}from "react";
+import React, { Component } from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import PostStatusFilter from "../post-status-filter";
 import PostList from "../post-list/post-list";
 import PostAddForm from "../post-add-form/post-add-form";
 
-import './app.css'
+import "./app.css";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const AppBlock = styled.div`
   margin: 0 auto;
   max-width: 800px;
-`
-
+`;
 
 export default class App extends Component {
-      constructor(props) {
-         super(props);
-         this.state = {
-            data: [
-               {label:" Going to learn React",important: true,id:'a1'},  
-               {label:"Going to home",important: false, id:"a2"},
-               {label:"We work",important: true,id:"a3"},
-               {label:"Batman - who is",important: false, id:"a4"},
-               {label:"Down or up",important: true,id:"a5"}
-            ]
-         };
-         this.deleteItem =this.deleteItem.bind(this);
-      }
-      deleteItem(id){
-         console.log(id)
-      }
-      render(){
-         return(
-            <AppBlock>
-                 <AppHeader/>
-              <div className="searc-panel d-flex">
-              <SearchPanel/>
-              <PostStatusFilter/>
-             </div>
-             <PostList 
-                     posts ={this.state.data}
-                     onDelete={this.deleteItem}/>
-             <PostAddForm/>
-            </AppBlock>
-         )
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { label: " Going to learn React", important: true, id: "a1" },
+        { label: "Going to home", important: false, id: "a2" },
+        { label: "We work", important: true, id: "a3" },
+        { label: "Batman - who is", important: false, id: "a4" },
+        { label: "Down or up", important: true, id: "a5" },
+      ],
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  deleteItem(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+      return {
+        data: newArr,
+      };
+    });
+  }
+  render() {
+    return (
+      <AppBlock>
+        <AppHeader />
+        <div className="searc-panel d-flex">
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList posts={this.state.data} onDelete={this.deleteItem} />
+        <PostAddForm />
+      </AppBlock>
+    );
+  }
 }
- 
