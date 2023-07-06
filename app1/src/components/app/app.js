dcdb56c588c06ee87d2f1f35f9a592a88c94136f
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-06 20:29:27 +0300

    Update app.js

--- app1/src/components/app/app.js
+++ app1/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {Component}from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import PostStatusFilter from "../post-status-filter";
@@ -15,32 +15,37 @@ const AppBlock = styled.div`
 `
 
 
-const App = () =>{
-
-   const data = [
-      {label:" Going to learn React",important: true,id:'a1'},  
-      {label:"Going to home",important: false, id:"a2"},
-      {label:"We work",important: true,id:"a3"},
-      {label:"Batman - who is",important: false, id:"a4"},
-      {label:"Down or up",important: true,id:"a5"}
-   ];
-  
-
-
-
-    return(
-       <AppBlock>
-            <AppHeader/>
-         <div className="searc-panel d-flex">
-         <SearchPanel/>
-         <PostStatusFilter/>
-        </div>
-        <PostList posts ={data}/>
-        <PostAddForm/>
-       </AppBlock>
-        
-       
-
-    )
+export default class App extends Component {
+      constructor(props) {
+         super(props);
+         this.state = {
+            data: [
+               {label:" Going to learn React",important: true,id:'a1'},  
+               {label:"Going to home",important: false, id:"a2"},
+               {label:"We work",important: true,id:"a3"},
+               {label:"Batman - who is",important: false, id:"a4"},
+               {label:"Down or up",important: true,id:"a5"}
+            ]
+         };
+         this.deleteItem =this.deleteItem.bind(this);
+      }
+      deleteItem(id){
+         console.log(id)
+      }
+      render(){
+         return(
+            <AppBlock>
+                 <AppHeader/>
+              <div className="searc-panel d-flex">
+              <SearchPanel/>
+              <PostStatusFilter/>
+             </div>
+             <PostList 
+                     posts ={this.state.data}
+                     onDelete={this.deleteItem}/>
+             <PostAddForm/>
+            </AppBlock>
+         )
+      }
 }
-export default App;
+ 
--- app1/src/components/post-list-item/post-list-item.js
+++ app1/src/components/post-list-item/post-list-item.js
@@ -10,22 +10,20 @@ export default class PostListItem extends Component {
     };
     this.onImportant = this.onImportant.bind(this);
     this.onLike = this.onLike.bind(this);
-  }
-
- 
+  } 
     onImportant() {
         this.setState(({ important }) => ({
         important: !important
         }));
     }
-
+    
     onLike() {
         this.setState(({ like }) => ({
         like: !like
         }));
     }
   render() {
-    const { label } = this.props;
+    const { label, onDelete} = this.props;
     const { important,like } = this.state;
     let classNames = "app-list-item d-flex justify-content-between";
     if (important) {
@@ -48,7 +46,8 @@ export default class PostListItem extends Component {
             onClick={this.onImportant}>
             <i className="bi bi-star"></i>
           </button>
-          <button type="button" className="btn-trash btn-sm">
+          <button type="button" className="btn-trash btn-sm"
+          onClick={onDelete}>
             <i className="bi bi-trash"></i>
           </button>
           <i className="bi bi-heart"></i>
