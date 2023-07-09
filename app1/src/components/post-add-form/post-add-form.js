fbfd49da1bddc19af2dc69f593caeda855bab77f
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-09 20:46:38 +0300

    Update post-add-form - Create function on submit

--- app1/src/components/post-add-form/post-add-form.js
+++ app1/src/components/post-add-form/post-add-form.js
@@ -4,15 +4,26 @@ import "./post-add-form.css";
 export default class PostAddForm extends Component{
     constructor(props){
         super(props);
+        this.state ={
+            text : ''
+        }
         this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
     onValueChange(e){
-        console.log(e.target.value);
+        this.setState({
+            text: e.target.value
+        })
     }
-
+    onSubmit(e){
+        e.preventDefault();
+        this.props.onAdd(this.state.text)
+    }
     render(){
         return(
-            <form className="bottom-panel bottom-panel-input d-flex">
+            <form 
+            className="bottom-panel bottom-panel-input d-flex"
+            onSubmit={this.onSubmit}>
                 <input
                     type="text"
                     placeholder="What are you thinking about now"
